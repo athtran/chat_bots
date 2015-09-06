f07fec510dbe712f445236fa06d42d1b3f633206
Alan Tran <alan.trann@gmail.com>
2015-09-05 18:26:47 -0700

    Fixed jquery script link

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
@@ -17,6 +17,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(port, function(){
+server.listen(port, function(){
   console.log('listening on *:' + port);
 });
