afb0d78e71813f2ba67ab17a28f55a6392254b7f
Alan Tran <alan.trann@gmail.com>
2015-09-05 22:12:01 -0700

    removed bot.js

--- bot.js
+++
@@ -1,7 +0,0 @@
-;(function () {
-  console.log("hello");
-  // var irc = require('irc');
-  // var client = new irc.Client('irc.yourserver.com', 'myNick', {
-  //     channels: ['#channel'],
-  // });
-})();
