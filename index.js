b4ebee34000035b938a44f0ac8597acaaefc11f3
Alan Tran <alan.trann@gmail.com>
2015-09-05 18:36:20 -0700

    added username

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
+var users = 0;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  io.emit('chat message', 'User #' + (++users) + ' has joined the server');
+  var username = "User " + users;
+
   console.log('a user has connected');
   socket.on('chat_message', function (message) {
+    message = username + ": " + message;
     io.emit('chat_message', message);
   });
   socket.on('disconnect', function () {
