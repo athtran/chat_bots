b39aeb564f8bcb476a988305e12c83e9fb995bc4
Alan Tran <alan.trann@gmail.com>
2015-09-05 22:11:36 -0700

    added random word spamming and blacklists

---
+++ bot.js
@@ -0,0 +1,7 @@
+;(function () {
+  console.log("hello");
+  // var irc = require('irc');
+  // var client = new irc.Client('irc.yourserver.com', 'myNick', {
+  //     channels: ['#channel'],
+  // });
+})();
--- index.js
+++ index.js
@@ -3,24 +3,36 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 var users = 0;
+var fs = require('fs');
+var words = [];
+var blacklist = {};
+
+fs.readFile('dictionary.txt', function(err, data) {
+    if(err) throw err;
+    words = data.toString().split("\n");
+});
+
+fs.readFile('blacklist.txt', function(err, data) {
+    if(err) throw err;
+    var arr = data.toString().split("\n");
+    for (var i = 0; i < arr.length; i++) {
+      blacklist[arr[i]] = true;
+    }
+});
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-
   io.emit('chat_message', 'User #' + (++users) + ' has joined the server');
-  var username = "User " + users;
-  //
-  // var id = setInterval(function() {
-  //   io.emit('chat_message', "test");
-  // }, 50);
+  var id = setInterval(function() {
+    broadcast_msg(words[Math.floor(Math.random()*words.length)]);
+  }, 5);
 
   console.log('a user has connected');
   socket.on('chat_message', function (message) {
-    message = username + ": " + message;
-    io.emit('chat_message', message);
+    broadcast_msg(message);
   });
   socket.on('disconnect', function () {
     console.log('a user has disconnected');
@@ -30,3 +42,14 @@ io.on('connection', function(socket){
 server.listen(port, function(){
   console.log('listening on *:' + port);
 });
+
+function broadcast_msg(message) {
+  if (blacklist[message]) {
+    io.emit('chat_message', "FORBIDDEN WORD");
+    console.log('FORBIDDEN WORD CAUGHT: ', message);
+  } else {
+    var username = "User " + users;
+    message = username + ": " + message;
+    io.emit('chat_message', message);
+  }
+}
